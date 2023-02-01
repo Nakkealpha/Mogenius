9f3fca9146ca6d052347c2cf49f8dce0930d4404
Nakkealpha <93373647+Nakkealpha@users.noreply.github.com>
2023-02-01 06:40:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'GreyMatters_Bot'
 
 #Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
-os.system("git clone https://GITHUB-USERNAME:token@github.com/GITHUB-USERNAME/YOUR-REPO-NAME okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://nakkealpha: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@github.com/Nakkealpha/File-Sharing-Bot okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
